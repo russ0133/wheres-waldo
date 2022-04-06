334bc1fa31fa54a633c51e321e520a610fac373a
Gabriel <gabrieldsfs@gmail.com>
2022-04-06 15:22:45 +0500

    feat: implement typescript on components StartScreen, GetUserData and App

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ const App: React.FC = () => {
 
   // Authentication and Routers
   const [user, loading] = useAuthState(auth);
-  const [uid, setUid] = useState<string | 0>(0);
+  const [uid, setUid] = useState<string | 1>(0);
 
   const navigate = useNavigate();
 
